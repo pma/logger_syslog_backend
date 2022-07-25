14811f2bf35f8f37b994615c03ee613d1ced5687
Paulo Almeida <paulo.almeida@timeboxed.co.ao>
2022-07-25 14:24:23 +0100

    Prepare v1.0.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LoggerSyslogBackend.Mixfile do
 
   def project do
     [app: :logger_syslog_backend,
-     version: "1.0.1",
+     version: "1.0.2",
      elixir: "~> 1.5",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
